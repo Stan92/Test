9d3d3ffc3efd8d8609ad150d464718d441ce3fe0
Stan Sainte-Rose <stan@iMac-de-Stan.local>
2012-03-18 21:50:26 +0100

    Initial Commit

---
+++ test1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by Stan Sainte-Rose on 18/03/12.
+//  Copyright (c) 2012 Netsunset. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
